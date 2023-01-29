12434ef594fcd95f811ec8f48190f91298fc4b42
ansadeso <ansadeso@gmail.com>
2023-01-29 15:45:49 +0200

    add scripts to all file

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function loadXMLDoc() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("demo").innerHTML =
+        document.getElementById("nav").innerHTML =
         this.responseText;
       }
     };
